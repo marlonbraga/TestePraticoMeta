328bb51b6fec2fb452a9ddfab00f73cc77a780f9
Marlon Braga <marlon@marlonbraga.com.br>
2022-04-11 18:06:07 -0300

    Implanta swagger

---
+++ TestePraticoMeta.API/Controllers/CalculaJurosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TestePraticoMeta.Domain;
+
+namespace TestePraticoMeta.API.Controllers
+{
+    [Route("calculajuros")]
+    [ApiController]
+    public class CalculaJurosController : ControllerBase
+    {
+        /// <summary>
+        /// GetByQueryParameters
+        /// </summary>
+        /// <returns> Calc of Interest</returns>
+        /// <remarks>
+        ///
+        /// Sample request
+        /// GET calculajuros?valorinicial=100&meses=5
+        ///
+        /// </remarks>
+        // GET calculajuros?valorinicial=100&meses=5
+        [HttpGet()]
+        public async Task<IActionResult> GetByQueryParameters(double valorinicial, int meses)
+        {
+            double value = valorinicial;
+            int time = meses;
+            Interest interest = new Interest();
+            string result = interest.CalcInterest(value, time).ToString("0.00");
+            return new OkObjectResult(result);
+        }
+    }
+}
---
+++ TestePraticoMeta.API/Controllers/TaxaDeJurosController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TestePraticoMeta.Domain;
+
+namespace TestePraticoMeta.API.Controllers
+{
+    [Route("taxadejuros")]
+    [ApiController]
+    public class TaxaDeJurosController : ControllerBase
+    {
+        // GET taxadejuros
+        [HttpGet]
+        public async Task<IActionResult> GetLatest()
+        {
+            Interest interest = new Interest();
+            string result = interest.GetInterestRate().ToString();
+            return new OkObjectResult(result);
+        }
+    }
+}
